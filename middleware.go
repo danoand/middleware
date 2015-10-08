a9f180dd4667eb7ae0b09e609fc18af3ee2735d8
Dan Anderson <danandersonmobile@yahoo.com>
2015-10-08 11:13:52 -0500

    Add funcs to validate POSTS and GETS

--- middleware.go
+++ middleware.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
@@ -37,19 +38,42 @@ func recoverHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(fn)
 }
 
-// Middleware handler function that will pass on a POST request but halt with any other request
-func passPOST(next http.Handler) http.Handler {
-	log.Printf("DEBUG: In the passPOST middleware function.\n")
+// Middleware handler function that will validate that a request is a POST and return if it is not
+func valPOST(next http.Handler) http.Handler {
+	m := "POST"
 
 	// Define a function that passes on a POST but returns for any other type of request
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		// Check the existing request method
-		if r.Method == "POST" {
+		if r.Method == m {
 			// Request method is a post, continue down the chain of middleware
 			next.ServeHTTP(w, r)
 		} else {
 			// Request is NOT a post return
-			log.Printf("INFO: Request from %v is not a POST.  Not continuing.\n", r.URL.String())
+			log.Printf("INFO: Request from %v is not a %v.  Not continuing.\n", r.URL.String(), m)
+			// Reply to the request with an Error
+			http.Error(w, fmt.Sprintf("STATUS = Method [%v] not allowed.", r.Method), http.StatusMethodNotAllowed)
+		}
+	}
+
+	return http.HandlerFunc(fn)
+}
+
+// Middleware handler function that will validate that a request is a GET and return if it is not
+func valGET(next http.Handler) http.Handler {
+	m := "GET"
+
+	// Define a function that passes on a GET but returns for any other type of request
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		// Check the existing request method
+		if r.Method == m {
+			// Request method is a post, continue down the chain of middleware
+			next.ServeHTTP(w, r)
+		} else {
+			// Request is NOT a post return
+			log.Printf("INFO: Request from %v is not a %v.  Not continuing.\n", r.URL.String(), m)
+			// Reply to the request with an Error
+			http.Error(w, fmt.Sprintf("STATUS = Method [%v] not allowed.", r.Method), http.StatusMethodNotAllowed)
 		}
 	}
 
